ce5e21990573ef6b1aa06075314aac11b5eb18eb
QMoayedpour <quentin.moayedpour@ensae.fr>
2025-01-04 16:06:36 +0000

    change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="CPD_L0_Penalty",
+    name="PrototypeProject",
     version="0.1.0",
     packages=find_packages(),
     install_requires=["pandas", "torch", "numpy",
